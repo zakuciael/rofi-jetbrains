fecd4f404cab842b535d4f75f7f68d69238774b2
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2023-09-28 19:10:14 +0200

    chore: stub rofi plugin

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,54 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use glib::{debug, GlibLogger, GlibLoggerDomain, GlibLoggerFormat};
+use log::LevelFilter;
+use rofi_mode::{export_mode, Action, Api, Event, Matcher};
+
+use crate::config::Config;
+
+mod config;
+mod rofi;
+mod traits;
+
+pub static G_LOG_DOMAIN: &str = "Modes.JetBrains";
+
+static GLIB_LOGGER: GlibLogger =
+  GlibLogger::new(GlibLoggerFormat::Plain, GlibLoggerDomain::CrateTarget);
+
+export_mode!(Mode<'_>);
+
+struct Mode<'rofi> {
+  api: Api<'rofi>,
+  config: Config,
+  entries: Vec<String>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl<'rofi> rofi_mode::Mode<'rofi> for Mode<'rofi> {
+  const NAME: &'static str = "jetbrains\0";
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+  fn init(api: Api<'rofi>) -> Result<Self, ()> {
+    log::set_logger(&GLIB_LOGGER).map_err(|_| ())?;
+    log::set_max_level(LevelFilter::Debug);
+    debug!("Initializing..");
+
+    Ok(Self {
+      api,
+      config: Config::default(),
+      entries: vec!["Test str".to_string()],
+    })
+  }
+
+  fn entries(&mut self) -> usize {
+    self.entries.len()
+  }
+
+  fn entry_content(&self, line: usize) -> rofi_mode::String {
+    self.entries[line].clone().into()
+  }
+
+  fn react(&mut self, event: Event, input: &mut rofi_mode::String) -> Action {
+    Action::Exit
+  }
+
+  fn matches(&self, line: usize, matcher: Matcher<'_>) -> bool {
+    matcher.matches(self.entries[line].as_str())
+  }
 }
