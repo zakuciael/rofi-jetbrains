9e795136340fc7127f25a07aae93ca44aabf3908
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2023-10-02 11:54:24 +0200

    refactor: make error handling cleaner by removing panic calls

--- src/error.rs
+++ src/error.rs
@@ -3,28 +3,28 @@ use glib::error;
 use crate::G_LOG_DOMAIN;
 
 pub trait UnwrapOrError<T> {
-  fn unwrap_or_error(self, msg: &str) -> T;
+  fn unwrap_or_error(self, msg: &str) -> Result<T, ()>;
 }
 
 impl<T> UnwrapOrError<T> for Option<T> {
-  fn unwrap_or_error(self, msg: &str) -> T {
+  fn unwrap_or_error(self, msg: &str) -> Result<T, ()> {
     match self {
-      Some(v) => v,
+      Some(v) => Ok(v),
       None => {
         error!("{}", msg);
-        panic!()
+        Err(())
       }
     }
   }
 }
 
 impl<T, E> UnwrapOrError<T> for Result<T, E> {
-  fn unwrap_or_error(self, msg: &str) -> T {
+  fn unwrap_or_error(self, msg: &str) -> Result<T, ()> {
     match self {
-      Ok(v) => v,
+      Ok(v) => Ok(v),
       Err(_) => {
         error!("{}", msg);
-        panic!()
+        Err(())
       }
     }
   }
