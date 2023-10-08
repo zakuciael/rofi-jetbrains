cad4d363a24e5cb861d438e0bf04623ba4405eab
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2023-10-08 22:01:29 +0200

    feat: add IDE aliases support

--- src/config.rs
+++ src/config.rs
@@ -1,23 +1,61 @@
 use std::path::{Path, PathBuf};
 
+use glib::warn;
+use itertools::Itertools;
 use resolve_path::PathResolveExt;
 
+use crate::ide::IDEType;
+use crate::macros::ensure_result;
 use crate::rofi::xrmoptions::config_parse_option;
+use crate::G_LOG_DOMAIN;
 
 static ROFI_CONFIG_PREFIX: &str = "jetbrains-";
 
 #[derive(Debug)]
 pub struct Config {
   pub install_dir: PathBuf,
+  pub custom_aliases: Vec<(String, IDEType)>,
 }
 
 impl Config {
   pub fn from_rofi() -> Self {
-    // TODO: Add configuration option for IDE aliases
     let install_dir: Option<PathBuf> = config_parse_option(
       &(ROFI_CONFIG_PREFIX.to_owned() + "install-dir"),
       "A path to the directory where all IDEs are installed",
     );
+
+    let custom_aliases = config_parse_option::<Option<Vec<String>>>(
+      &(ROFI_CONFIG_PREFIX.to_owned() + "custom-aliases"),
+      "A rofi list declaring custom IDE aliases",
+    )
+    .unwrap_or_default();
+
+    let custom_aliases = custom_aliases
+      .into_iter()
+      .map(|raw| -> Result<_, _> {
+        let (alias, product_code) = ensure_result!(
+          raw.split(':').collect_tuple::<(&str, &str)>(),
+          "Failed to parse custom alias, {:?} is not a valid alias",
+          raw
+        );
+
+        let ide_type = ensure_result!(
+          IDEType::from_product_code(product_code),
+          "Failed to parse {:?} alias, {:?} is an unknown IDE",
+          alias,
+          product_code
+        );
+
+        Ok((alias.to_owned(), ide_type))
+      })
+      .filter_map(|res| match res {
+        Ok(v) => Some(v),
+        Err(err) => {
+          warn!("{}", err);
+          None
+        }
+      })
+      .collect::<Vec<_>>();
 
     Self {
       install_dir: install_dir.unwrap_or_else(|| {
@@ -25,6 +63,7 @@ impl Config {
           .resolve()
           .to_path_buf()
       }),
+      custom_aliases,
     }
   }
 }
--- src/ide/mod.rs
+++ src/ide/mod.rs
@@ -1,18 +1,20 @@
+use std::fmt::{Display, Formatter};
+
+use strum::EnumIter;
+
 pub mod data;
 mod de;
 pub mod product_info;
 pub mod properties;
 
 #[allow(clippy::upper_case_acronyms)]
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash, EnumIter)]
 pub enum IDEType {
   Aqua,
   CLion,
   IntelliJIDEA,
-  IntelliJIDEACommiunity,
   PHPStorm,
   PyCharm,
-  PyCharmCommiunity,
   Rider,
   WebStorm,
   GoLand,
@@ -33,12 +35,10 @@ impl IDEType {
       "RR" => IDEType::RustRover,
       "WS" => IDEType::WebStorm,
       "RM" => IDEType::RubyMine,
-      "PC" => IDEType::PyCharmCommiunity,
-      "PY" => IDEType::PyCharm,
+      "PY" | "PC" => IDEType::PyCharm,
       "PS" => IDEType::PHPStorm,
       "MPS" => IDEType::MPS,
-      "IU" => IDEType::IntelliJIDEA,
-      "IC" => IDEType::IntelliJIDEACommiunity,
+      "IU" | "IC" => IDEType::IntelliJIDEA,
       "DS" => IDEType::DataSpell,
       "DB" => IDEType::DataGrip,
       "QA" => IDEType::Aqua,
@@ -47,5 +47,49 @@ impl IDEType {
     };
 
     Some(enum_value)
+  }
+
+  pub fn get_default_alias(&self) -> (String, IDEType) {
+    let alias = match self {
+      IDEType::Aqua => "aqua",
+      IDEType::CLion => "clion",
+      IDEType::IntelliJIDEA => "idea",
+      IDEType::PHPStorm => "phpstorm",
+      IDEType::PyCharm => "pycharm",
+      IDEType::Rider => "rider",
+      IDEType::WebStorm => "webstorm",
+      IDEType::GoLand => "goland",
+      IDEType::DataGrip => "datagrip",
+      IDEType::DataSpell => "dataspell",
+      IDEType::RubyMine => "rubymine",
+      IDEType::AndroidStudio => "android-studio",
+      IDEType::RustRover => "rustrover",
+      IDEType::MPS => "mps",
+    };
+
+    (alias.to_owned(), self.clone())
+  }
+}
+
+impl Display for IDEType {
+  fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+    let name = match self {
+      IDEType::Aqua => "Aqua",
+      IDEType::CLion => "CLion",
+      IDEType::IntelliJIDEA => "IntelliJ IDEA",
+      IDEType::PHPStorm => "PHPStorm",
+      IDEType::PyCharm => "PyCharm",
+      IDEType::Rider => "Rider",
+      IDEType::WebStorm => "WebStorm",
+      IDEType::GoLand => "GoLand",
+      IDEType::DataGrip => "DataGrip",
+      IDEType::DataSpell => "DataSpell",
+      IDEType::RubyMine => "RubyMine",
+      IDEType::AndroidStudio => "Android Studio",
+      IDEType::RustRover => "RustRover",
+      IDEType::MPS => "MPS",
+    };
+
+    write!(f, "{}", name)
   }
 }
