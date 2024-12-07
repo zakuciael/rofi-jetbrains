0343707a7de5aabac60f34e73b8ac01572dce6e6
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2024-12-07 22:42:27 +0100

    chore(flake): add "smort" wrapper around `cargo`
    Due to a bug in RustRover if `rustup` is not set as the project toolchain, it will warn user each time it performs a check and default to using `cargo check` anyway. Using this wrapper I can trick RustRover into using `clippy` by overriding the `cargo check` command.

--- shell.nix
+++ shell.nix
@@ -1,23 +1,42 @@
 {
   crane,
   callPackage,
+  writeShellApplication,
   rustup,
-}:
-crane.devShell {
-  name = "rofi-jetbrains";
-  inputsFrom = [
-    (callPackage ./. {inherit crane;})
-  ];
+}: let
+  rustc = crane.rustc;
+  wrapper = writeShellApplication {
+    name = "cargo-wrapper";
+    text = ''
+      if [ "$1" == "check" ]; then
+      	${rustc}/bin/cargo clippy "''${@:2}"
+      else
+      	${rustc}/bin/cargo "$@"
+      fi
+    '';
+  };
+  toolchain = crane.rustc.overrideAttrs (prev: {
+    buildCommand =
+      prev.buildCommand
+      + ''
+        cp -f ${wrapper}/bin/cargo-wrapper $out/bin/cargo
+      '';
+  });
+in
+  crane.devShell {
+    name = "rofi-jetbrains";
+    inputsFrom = [
+      (callPackage ./. {inherit crane;})
+    ];
 
-  packages = [
-    rustup
-  ];
+    packages = [
+      rustup
+    ];
 
-  shellHook = ''
-    if [ ! -d .direnv/links/rustup ]; then
-    	mkdir -p .direnv/links/
-    	ln -sf "${crane.rustc}" .direnv/links/rust
-    	ln -sf "${rustup}" .direnv/links/rustup
-    fi
-  '';
-}
+    shellHook = ''
+      if [ ! -d .direnv/links/rust ]; then
+      	mkdir -p .direnv/links/
+      	ln -sf "${toolchain}" .direnv/links/rust
+      fi
+    '';
+  }
