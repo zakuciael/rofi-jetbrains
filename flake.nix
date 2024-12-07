45e368aa05ad54a3cf3c40dca48075555726f966
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2024-12-07 19:09:21 +0100

    refactor(flake): switch `flake-parts` helper

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -16,52 +16,63 @@
   };
 
   outputs = {
-    self,
     nixpkgs,
-    flake-utils,
+    flake-parts,
+    fenix,
     ...
-  } @ inputs: let
-    systems = ["x86_64-linux" "aarch64-linux" "i686-linux"];
-  in
-    flake-utils.lib.eachSystem systems (system: let
-      lib = nixpkgs.lib;
-      pkgs = import nixpkgs {
-        inherit system;
-        config = {};
-        overlays = [
-          (final: prev: {
-            fenix = import inputs.fenix {
-              pkgs = prev;
-              rust-analyzer-src = throw "not used";
-            };
-          })
-        ];
-      };
-      toolchain = pkgs.fenix.stable;
-    in rec {
-      devShells.default = import ./shell.nix {inherit lib pkgs toolchain;};
-      packages = {
-        default = packages.rofi-jetbrains;
-        rofi-jetbrains = import ./. {inherit lib pkgs toolchain;};
-      };
-      apps = let
-        mkRofiPackage = pkg:
-          if builtins.hasAttr "override" pkg
-          then pkg.override (old: {plugins = (old.plugins or []) ++ [packages.rofi-jetbrains];})
-          else pkg;
-      in {
-        default =
-          if builtins.getEnv "WAYLAND_DISPLAY" == ""
-          then apps.rofi
-          else apps.rofi-wayland;
-        rofi-wayland = {
-          type = "app";
-          program = "${lib.getExe (mkRofiPackage pkgs.rofi-wayland)}";
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "i686-linux"];
+
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: let
+        lib = nixpkgs.lib;
+      in rec {
+        _module.args.pkgs = import nixpkgs {
+          inherit system;
+          config = {};
+          overlays = [fenix.overlays.default];
         };
-        rofi = {
-          type = "app";
-          program = "${lib.getExe (mkRofiPackage pkgs.rofi)}";
+
+        packages = {
+          default = packages.rofi-jetbrains;
+          rofi-jetbrains = import ./. {
+            inherit lib pkgs;
+            toolchain = pkgs.fenix.stable;
+          };
+        };
+
+        devShells = {
+          default = import ./shell.nix {
+            inherit lib pkgs;
+            toolchain = pkgs.fenix.stable;
+          };
+        };
+
+        apps = let
+          mkRofiPackage = pkg:
+            if builtins.hasAttr "override" pkg
+            then pkg.override (old: {plugins = (old.plugins or []) ++ [packages.rofi-jetbrains];})
+            else pkg;
+        in {
+          default =
+            if builtins.getEnv "WAYLAND_DISPLAY" == ""
+            then apps.rofi
+            else apps.rofi-wayland;
+          rofi-wayland = {
+            type = "app";
+            program = "${lib.getExe (mkRofiPackage pkgs.rofi-wayland)}";
+            meta.description = "rofi-wayland cli with the `rofi-jetbrains` plugin pre-installed";
+          };
+          rofi = {
+            type = "app";
+            program = "${lib.getExe (mkRofiPackage pkgs.rofi)}";
+            meta.description = "rofi cli with the `rofi-jetbrains` plugin pre-installed";
+          };
         };
       };
-    });
+    };
 }
