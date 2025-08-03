b3d2eed4f2bc3a370faaad04510293ea32328b46
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2025-08-03 13:34:29 +0200

    chore(flake): add `act` pkg to dev-shell

--- shell.nix
+++ shell.nix
@@ -3,10 +3,13 @@
   callPackage,
   writeShellApplication,
   fetchFromGitHub,
-}: let
-  cargo-wrapper = let
-    cargo = crane.cargo;
-  in
+  act,
+}:
+let
+  cargo-wrapper =
+    let
+      inherit (crane) cargo;
+    in
     writeShellApplication {
       name = "cargo-wrapper";
       text = ''
@@ -18,47 +21,52 @@
       '';
     };
   toolchain = crane.rustc.overrideAttrs (prev: {
-    buildCommand =
-      prev.buildCommand
-      + ''
-        cp -f ${cargo-wrapper}/bin/cargo-wrapper $out/bin/cargo
-      '';
+    buildCommand = prev.buildCommand + ''
+      cp -f ${cargo-wrapper}/bin/cargo-wrapper $out/bin/cargo
+    '';
   });
-  knope = let
-    src = crane.cleanCargoSource (fetchFromGitHub {
-      owner = "knope-dev";
-      repo = "knope";
-      rev = "knope/v0.18.1";
-      hash = "sha256-KA5ePuN9MWbhsrz3UVr8brbs77P0AHXK/3f6RccfWac=";
-    });
-    commonArgs = {
-      inherit src;
-      inherit (crane.crateNameFromCargoToml {cargoToml = "${src}/crates/knope/Cargo.toml";}) pname version;
-      strictDeps = true;
-    };
-    cargoArtifacts = crane.buildDepsOnly commonArgs;
-  in
-    crane.buildPackage (commonArgs
+  knope =
+    let
+      src = crane.cleanCargoSource (fetchFromGitHub {
+        owner = "knope-dev";
+        repo = "knope";
+        rev = "knope/v0.18.1";
+        hash = "sha256-KA5ePuN9MWbhsrz3UVr8brbs77P0AHXK/3f6RccfWac=";
+      });
+      commonArgs = {
+        inherit src;
+        inherit (crane.crateNameFromCargoToml { cargoToml = "${src}/crates/knope/Cargo.toml"; })
+          pname
+          version
+          ;
+        strictDeps = true;
+      };
+      cargoArtifacts = crane.buildDepsOnly commonArgs;
+    in
+    crane.buildPackage (
+      commonArgs
       // {
         inherit cargoArtifacts;
         cargoExtraArgs = "-p knope";
         doCheck = false;
-      });
+      }
+    );
 in
-  crane.devShell {
-    name = "rofi-jetbrains";
-    inputsFrom = [
-      (callPackage ./. {inherit crane;})
-    ];
+crane.devShell {
+  name = "rofi-jetbrains";
+  inputsFrom = [
+    (callPackage ./. { inherit crane; })
+  ];
 
-    packages = [
-      knope
-    ];
+  packages = [
+    act
+    knope
+  ];
 
-    shellHook = ''
-      if [ ! -d .direnv/links/rust ]; then
-      	mkdir -p .direnv/links/
-      	ln -sf "${toolchain}" .direnv/links/rust
-      fi
-    '';
-  }
+  shellHook = ''
+    if [ ! -d .direnv/links/rust ]; then
+    	mkdir -p .direnv/links/
+    	ln -sf "${toolchain}" .direnv/links/rust
+    fi
+  '';
+}
